687c0efe990703509f35fe68fdd78f4f888e15f2
recca383 <sirpatrick121402@gmail.com>
2025-11-14 14:39:15 +0800

    Added Address Main Class

---
+++ src/Domain/Address/Address.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Address;
+public class Address
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string House_Number { get; set; }
+    public string Street_Name { get; set; }
+    public int Region_Id { get; set; }
+    public int Province_Id { get; set; }
+    public int CityMun_Id { get; set; }
+    public int Barangay_Id { get; set; }
+}
