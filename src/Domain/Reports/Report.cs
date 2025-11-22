6daca33161d16b955334f29018cf74e89684bfa8
recca383 <sirpatrick121402@gmail.com>
2025-11-22 16:55:30 +0800

    Changed category from class to enum

--- src/Domain/Reports/Report.cs
+++ src/Domain/Reports/Report.cs
@@ -11,11 +11,11 @@ public sealed class Report : Entity
     public string? Description { get; set; }
     public Location ReportedAt { get; set; }
     public Status Status { get; set; }
-    public Guid Category { get; set; }
+    public Category Category { get; set; }
     public bool IsDeleted { get; set; }
     public DateTime DateCreated { get; set; }
     public DateTime? DateLastUpdated { get; set; }
-    public DateTime? DateDeleted { get; set; }
+    public DateTime? DateResolved { get; set; }
     public Priority Priority { get; set; }
     
     
