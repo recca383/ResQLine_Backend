3908d45e748db27445abe0d35d818c58a6c2fa7a
recca383 <sirpatrick121402@gmail.com>
2025-12-01 22:52:17 +0800

    Added Otp configuration

---
+++ src/Infrastructure/Otps/OtpConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.OtpStores;
+using Domain.Users;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Otps;
+internal class OtpConfiguration : IEntityTypeConfiguration<OtpStore>
+{
+    public void Configure(EntityTypeBuilder<OtpStore> builder)
+    {
+        builder.HasKey(x => x.Id);
+    }
+}
+
+
