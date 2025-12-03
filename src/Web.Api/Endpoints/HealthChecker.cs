5f495a6fde910ddd618eb37c71b29421e013b7ff
recca383 <sirpatrick121402@gmail.com>
2025-12-03 15:31:51 +0800

    Added health checker

---
+++ src/Web.Api/Endpoints/HealthChecker.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Web.Api.Endpoints;
+
+public class HealthChecker : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/", () => "API is running!");
+    }
+}
