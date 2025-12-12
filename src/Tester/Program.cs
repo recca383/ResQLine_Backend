79a8b5c87454143cc5c6962dc7d7aee54cca8a03
recca383 <sirpatrick121402@gmail.com>
2025-12-13 01:06:18 +0800

    Added Tester console application

---
+++ src/Tester/Program.cs
@@ -0,0 +1,24 @@
+﻿using AI;
+
+namespace Tester;
+
+internal sealed class Program
+{
+    public static void Main(string[] args)
+    {
+        var imageClassification = new ImageClassification();
+
+        string path = Path.Combine(AppContext.BaseDirectory, "Model", "test.jpg");
+        byte[] imageBytes = File.ReadAllBytes(path);
+
+        HashSet<string> results = imageClassification.Predict(new List<byte[]> { imageBytes });
+        
+
+        foreach(string label in results)
+        {
+            Console.WriteLine($"Predicted label: {label}");
+        }
+
+        imageClassification.Dispose();
+    }
+}
