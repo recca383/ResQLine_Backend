38437350ef4487ef72c12098f8a6f5c83f09e818
recca383 <sirpatrick121402@gmail.com>
2025-12-12 18:50:24 +0800

    Changed report to contain multiple images

---
+++ src/Infrastructure/Database/Migrations/20251212104455_Store_Multiple_Images_Per_Report.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Database.Migrations;
+
+/// <inheritdoc />
+public partial class Store_Multiple_Images_Per_Report : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AlterColumn<List<byte[]>>(
+            name: "Image",
+            schema: "public",
+            table: "Reports",
+            type: "bytea[]",
+            nullable: false,
+            oldClrType: typeof(byte[]),
+            oldType: "bytea");
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AlterColumn<byte[]>(
+            name: "Image",
+            schema: "public",
+            table: "Reports",
+            type: "bytea",
+            nullable: false,
+            oldClrType: typeof(List<byte[]>),
+            oldType: "bytea[]");
+    }
+}
