405b77f65187d0627bec8cc7f7ad472ae78a0ada
recca383 <sirpatrick121402@gmail.com>
2025-12-13 01:10:03 +0800

    Completed register verify otp

--- src/Application/Otps/Register/Verify/VerifyRegisterOtpCommandHandler.cs
+++ src/Application/Otps/Register/Verify/VerifyRegisterOtpCommandHandler.cs
@@ -5,17 +5,50 @@ using System.Text;
 using System.Threading.Tasks;
 using Application.Abstractions.Data;
 using Application.Abstractions.Messaging;
+using Domain.OtpStores;
+using Domain.Users;
 using SharedKernel;
 
 namespace Application.Otps.Register.Verify;
 internal sealed class VerifyRegisterOtpCommandHandler(
-    //IApplicationDbContext context
+    IApplicationDbContext context
     ) :
     ICommandHandler<VerifyRegisterOtpCommand>
 {
-    public Task<Result> Handle(VerifyRegisterOtpCommand command, CancellationToken cancellationToken)
+    public async Task<Result> Handle(VerifyRegisterOtpCommand command, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        OtpStore storedOtp = context.OtpStores
+            .FirstOrDefault(otp => otp.MobileNumber == command.MobileNumber
+                                && otp.OtpType == OtpType.Register);
+
+        if(storedOtp is null)
+        {
+            return Result.Failure(OtpErrors.NoOtpRequested(command.MobileNumber));
+        }
+
+        if(storedOtp.Expiry < DateTime.UtcNow)
+        {
+            return Result.Failure(OtpErrors.OtpExpired);
+        }
+
+        if(storedOtp.Otp != command.Otp)
+        {
+            return Result.Failure(OtpErrors.OtpNotEqual);
+        }
+
+        var user = new User
+        {
+            Id = Guid.NewGuid(),
+            MobileNumber = command.MobileNumber,
+            FirstName = command.FirstName,
+            LastName = command.LastName,
+        };
+
+        context.Users.Add(user);
+        await context.SaveChangesAsync(cancellationToken);
+
+        return Result.Success();
+
     }
 }
 
