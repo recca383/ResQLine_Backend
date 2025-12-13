19ce1ba4322b296fdecabd93070112533e67b9f1
recca383 <sirpatrick121402@gmail.com>
2025-12-13 10:53:25 +0800

    changes on image class model, renamed Image into Images

--- src/Application/Reports/Get/ReportResponse.cs
+++ src/Application/Reports/Get/ReportResponse.cs
@@ -5,8 +5,8 @@ namespace Application.Reports.Get;
 public sealed class ReportResponse
 {
     public Guid Id { get; set; }
-    public string Description { get; set; }
-    public List<byte[]> Image { get; set; }
+    public string? Description { get; set; }
+    public List<byte[]> Images { get; set; }
     public Category Category { get; set; }
     public Location Location { get; set; }
     public DateTime CreatedAt { get; set; }
--- src/Application/Reports/GetById/ReportResponse.cs
+++ src/Application/Reports/GetById/ReportResponse.cs
@@ -5,9 +5,10 @@ namespace Application.Reports.GetById;
 public sealed class ReportResponse
 {
     public Guid Id { get; set; }
-    public List<byte[]> Image { get; set; }
-    public Category Category { get; set; }
     public string? Description { get; set; }
+    public List<byte[]> Images { get; set; }
+    public Category Category { get; set; }
     public Location Location { get; set; }
     public DateTime CreatedAt { get; set; }
+    public Status Status { get; set; }
 }
