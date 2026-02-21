d60f0f1d23348580a3f126215f03aecbde47a58c
recca383 <sirpatrick121402@gmail.com>
2026-02-22 01:41:48 +0800

    Created auth string requirements to be used on endpoint

---
+++ src/Web.Api/Endpoints/AuthRequirements.cs
@@ -0,0 +1,9 @@
+﻿namespace Web.Api.Endpoints;
+
+internal static class AuthRequirements
+{
+    public const string ADMIN = "Admin";
+    public const string RESPONDER_BFP = "Responder_BFP";
+    public const string RESPONDER_PNP = "Responder_PNP";
+    public const string RESPONDER_CTMO = "Responder_CTMO";
+}
