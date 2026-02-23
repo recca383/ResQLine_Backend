eb68913770883bfdd55fce57d686af5eaa85e600
recca383 <sirpatrick121402@gmail.com>
2026-02-24 02:22:02 +0800

    Disabled authorization for delete endpoint

--- src/Web.Api/Endpoints/Reports/Delete.cs
+++ src/Web.Api/Endpoints/Reports/Delete.cs
@@ -22,6 +22,7 @@ internal sealed class Delete : IEndpoint
             return result.Match(Results.NoContent, CustomResults.Problem);
         })
         .WithTags(Tags.Reports)
-        .RequireAuthorization();
+        //.RequireAuthorization()
+        ;
     }
 }
