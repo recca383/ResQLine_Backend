f09069b0893ac65734fd82ede398aede14e33f60
recca383 <sirpatrick121402@gmail.com>
2026-02-25 02:29:54 +0800

    Added enum extension And for flags

---
+++ src/SharedKernel/EnumExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedKernel;
+public static class EnumExtensions
+{
+    public static TFlags And<TFlags, TEnum>(this TFlags flags, TEnum value)
+    where TFlags : Enum
+    where TEnum : Enum
+    {
+        int result = Convert.ToInt32(flags, CultureInfo.InvariantCulture)
+                    & Convert.ToInt32(value, CultureInfo.InvariantCulture);
+
+        return (TFlags)Enum.ToObject(typeof(TFlags), result);
+    }
+}
