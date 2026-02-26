a4eb715984a3863627d9887acc0e65da37c05172
recca383 <sirpatrick121402@gmail.com>
2026-02-27 02:38:33 +0800

    Implemented notification endpoint

---
+++ src/Web.Api/Hubs/Notification.cs
@@ -0,0 +1,12 @@
+﻿using Application.Abstractions.Hubs;
+using Web.Api.Endpoints;
+
+namespace Web.Api.Hubs;
+
+public class Notification : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapHub<NotificationHub>("/hub/Notification");
+    }
+}
