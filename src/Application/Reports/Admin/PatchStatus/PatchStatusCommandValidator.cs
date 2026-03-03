501a14f34b4e9e8d24ec8587c96bec2fd4a46e57
recca383 <sirpatrick121402@gmail.com>
2026-03-03 17:27:07 +0800

    Implemented Patch status validator

---
+++ src/Application/Reports/Admin/PatchStatus/PatchStatusCommandValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace Application.Reports.Complete;
+
+internal sealed class PatchStatusCommandValidator : AbstractValidator<ResolveReportCommand>
+{
+    public PatchStatusCommandValidator()
+    {
+        RuleFor(c => c.ReportId).NotEmpty();
+    }
+}
