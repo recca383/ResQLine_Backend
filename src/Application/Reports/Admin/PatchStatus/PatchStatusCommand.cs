779cfd043147594f351153b1424c0f15cb706644
recca383 <sirpatrick121402@gmail.com>
2026-03-03 17:26:43 +0800

    Created patch status command

---
+++ src/Application/Reports/Admin/PatchStatus/PatchStatusCommand.cs
@@ -0,0 +1,7 @@
+﻿using Application.Abstractions.Messaging;
+using Domain.Reports;
+
+namespace Application.Reports.Complete;
+
+public sealed record PatchStatusCommand
+    (Guid ReportId, Status Status) : ICommand;
