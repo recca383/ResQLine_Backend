0ccb040183fde9bdf600e2bbd5f5a61d1ac609b5
recca383 <sirpatrick121402@gmail.com>
2026-03-04 17:28:12 +0800

    Implemented patch status endpoint

---
+++ src/Web.Api/Endpoints/Reports/Admin/PatchStatus.cs
@@ -0,0 +1,36 @@
+﻿
+using Application.Abstractions.Messaging;
+using Application.Reports.Complete;
+using Domain.Reports;
+using Microsoft.AspNetCore.Mvc;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.Reports.Admin;
+
+internal sealed class PatchStatus : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapPatch("admin/reports/{id:guid}/status", async (
+            Guid id,
+            [FromBody]Status status,
+            ICommandHandler<PatchStatusCommand> handler,
+            CancellationToken cancellationToken) =>
+        {
+            var command = new PatchStatusCommand(id,status);
+
+            Result result = await handler.Handle(command, cancellationToken);
+
+            return result.Match(Results.NoContent, CustomResults.Problem);
+        })
+        .WithTags(Tags.Admin)
+        //.RequireAuthorization(
+        //    AuthRequirements.RESPONDER_PNP,
+        //    AuthRequirements.RESPONDER_BFP,
+        //    AuthRequirements.RESPONDER_CTMO,
+        //    AuthRequirements.ADMIN)
+        ;
+    }
+}
