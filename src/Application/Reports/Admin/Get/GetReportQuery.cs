0a056223be7b0c2d34bcea7c537a27cc6042f5fc
recca383 <sirpatrick121402@gmail.com>
2026-03-05 17:30:14 +0800

    Created Get report query

---
+++ src/Application/Reports/Admin/Get/GetReportQuery.cs
@@ -0,0 +1,13 @@
+﻿using Application.Abstractions.Messaging;
+
+namespace Application.Reports.Admin.Get;
+
+public sealed record GetReportQuery(
+    string sort,
+    int pageSize,
+    int pageoffset
+    
+    
+    ) : IQuery<List<ReportResponse>>;
+
+
