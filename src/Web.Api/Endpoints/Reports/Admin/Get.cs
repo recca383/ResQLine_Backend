2d050b650fd5eb1c655caaf846dec438231c257f
recca383 <sirpatrick121402@gmail.com>
2026-03-05 17:31:57 +0800

    Implemented Get admin report endpoint

---
+++ src/Web.Api/Endpoints/Reports/Admin/Get.cs
@@ -0,0 +1,38 @@
+﻿
+using Application.Abstractions.Messaging;
+using Application.Reports.Admin.Get;
+using SharedKernel;
+using Web.Api.Endpoints.Requests;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.Reports.Admin;
+
+internal sealed class Get : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("admin/reports", async (
+            [AsParameters]GetRequest request,
+            IQueryHandler<GetReportQuery, List<ReportResponse>> handler,
+            CancellationToken cancellationToken) =>
+        {
+            var query = new GetReportQuery(
+                                    request.sort,
+                                    request.pageSize,
+                                    request.pageoffset
+                                    );
+
+            Result<List<ReportResponse>> result = await handler.Handle(query, cancellationToken);
+
+            return result.Match(Results.Ok, CustomResults.Problem);
+        })
+        .WithTags(Tags.Admin)
+        //.RequireAuthorization(
+        //    AuthRequirements.RESPONDER_PNP,
+        //    AuthRequirements.RESPONDER_BFP,
+        //    AuthRequirements.RESPONDER_CTMO,
+        //    AuthRequirements.ADMIN)
+        ;
+    }
+}
