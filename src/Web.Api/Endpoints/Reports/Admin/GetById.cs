0146c261b08891659eab7cbb0fa58b01bc55b4fc
recca383 <sirpatrick121402@gmail.com>
2026-03-06 17:33:14 +0800

    implemented get by id endpoint

---
+++ src/Web.Api/Endpoints/Reports/Admin/GetById.cs
@@ -0,0 +1,29 @@
+﻿
+using Application.Abstractions.Messaging;
+using Application.Reports.Admin.GetById;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.Reports.Admin;
+
+internal sealed class GetById : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("admin/reports/{id:guid}", async (
+            Guid id,
+            IQueryHandler<GetReportByIdQuery, ReportResponse> handler,
+            CancellationToken cancellationToken) =>
+        {
+            var command = new GetReportByIdQuery(id);
+
+            Result<ReportResponse> result = await handler.Handle(command, cancellationToken);
+
+            return result.Match(Results.Ok, CustomResults.Problem);
+        })
+        .WithTags(Tags.Admin)
+        //.RequireAuthorization()
+        ;
+    }
+}
