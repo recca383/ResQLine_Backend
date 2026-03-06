ded2a3a94fc4ace2906ff0fd5309d98101b41c97
recca383 <sirpatrick121402@gmail.com>
2026-03-06 17:32:57 +0800

    Created report response for dto

---
+++ src/Application/Reports/Admin/GetById/ReportResponse.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Reports;
+
+namespace Application.Reports.Admin.GetById;
+
+public sealed class ReportResponse
+{
+    public Guid Id { get; set; }
+    public string? Description { get; set; }
+    public List<byte[]> Images { get; set; }
+    public Category Category { get; set; }
+    public Location Location { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public Status Status { get; set; }
+}
