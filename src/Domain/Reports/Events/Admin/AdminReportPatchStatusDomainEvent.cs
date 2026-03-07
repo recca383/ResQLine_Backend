dd7b7ef82838d1bc881a4546bbe4d3ed73e51ac1
recca383 <sirpatrick121402@gmail.com>
2026-03-07 21:06:26 +0800

    created domain event for report changed status by admin

---
+++ src/Domain/Reports/Events/Admin/AdminReportPatchStatusDomainEvent.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharedKernel;
+
+namespace Domain.Reports.Events.Admin;
+public sealed record AdminReportPatchStatusDomainEvent(Report report) : IDomainEvent;
