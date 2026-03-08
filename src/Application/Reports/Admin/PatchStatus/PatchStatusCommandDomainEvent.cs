adc40b297e46c45d569d8a9db3c34c2745fbd3f0
recca383 <sirpatrick121402@gmail.com>
2026-03-08 21:11:21 +0800

    Created the implementation for patch status domain event

---
+++ src/Application/Reports/Admin/PatchStatus/PatchStatusCommandDomainEvent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Abstractions.Hubs;
+using Domain.Reports.Events.Admin;
+using Microsoft.AspNetCore.SignalR;
+using SharedKernel;
+
+namespace Application.Reports.Admin.PatchStatus;
+internal sealed class PatchStatusCommandDomainEvent
+    (
+        IHubContext<NotificationHub> hubContext
+    ): IDomainEventHandler<AdminReportPatchStatusDomainEvent>
+{
+    public Task Handle(AdminReportPatchStatusDomainEvent domainEvent, CancellationToken cancellationToken)
+    {
+        hubContext.Clients.All.SendAsync("ReportStatusChanged", domainEvent.report, cancellationToken);
+
+        return Task.CompletedTask;
+    }
+}
